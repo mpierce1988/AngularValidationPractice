ce3b0eaef4f9d07359ce2184b64809f75d99e06f
mpierce1988 <mpierce1988@gmail.com>
2022-12-14 18:33:12 -0400

    Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuestDetailComponent } from './guest-detail/guest-detail.component';
+import { GuestsComponent } from './guests/guests.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/guests", pathMatch: "full"},
+  { path: "guests", component: GuestsComponent},
+  { path: "guests/:id", component: GuestDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
