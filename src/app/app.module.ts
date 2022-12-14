959e13dcde0592147be055323efae56f7bc8d651
mpierce1988 <mpierce1988@gmail.com>
2022-12-14 18:33:21 -0400

    imported bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GuestDetailComponent} from './guest-detail/guest-detail.component';
 import { GuestsComponent } from './guests/guests.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -16,7 +17,8 @@ import { GuestsComponent } from './guests/guests.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
